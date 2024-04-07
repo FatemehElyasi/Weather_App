e5951134a851a9c7821a4de2d9b78c41270ddd62
Fatemelyasi <fatemelyasii@gmail.com>
2024-04-07 23:34:07 +0330

    ADD Models

---
+++ app/src/main/java/ir/fatemelyasii/weather/model/DailyForecasts.kt
@@ -0,0 +1,8 @@
+package ir.fatemelyasii.weather.model
+
+import com.google.gson.annotations.SerializedName
+
+data class DailyForecasts(
+    @SerializedName("DailyForecasts")
+    val dailyForecasts: List<DailyForecast>
+)
