439b8cca960b0e9d13c1e4290b01df697a580a25
Fatemelyasi <fatemelyasii@gmail.com>
2024-04-09 02:35:50 +0330

    ADD-SETUP Navigation Screen

---
+++ app/src/main/java/ir/fatemelyasii/weather/view/HomeScreen.kt
@@ -0,0 +1,11 @@
+package ir.fatemelyasii.weather.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(
+    navController: NavController
+){
+
+}
